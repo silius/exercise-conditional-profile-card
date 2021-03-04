6ed0a4a33e3b6d50a78e07b0fdc6d2df77ebb2d1
Adrian Acosta <aacrealms@hotmail.com>
2021-03-04 01:39:40 +0000

    Delivers Conditional Profile

--- src/js/index.js
+++ src/js/index.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Jane " : variables.name} ${
+    variables.lastname == null ? "Doe" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "World ruler" : variables.role}</h2>
+          <h3>${variables.city == null ? "San Jose" : variables.city}, ${
+    variables.country == null ? "Costa Rica" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "alesanchezr" : variables.twitter
+            }"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "alesanchezr" : variables.github
+            }"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null ? "alesanchezr" : variables.linkedin
+            }"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null ? "alesanchezr" : variables.instagram
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
